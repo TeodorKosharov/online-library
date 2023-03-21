6456bd82dff69775c27d940f512533ccb3c6da5f
TeodorKosharov <teodor.kosharov@gmail.com>
2023-03-22 00:29:14 +0200

    Added genre field for book model

--- online_library_server/online_library_server/core/models.py
+++ online_library_server/online_library_server/core/models.py
@@ -7,6 +7,12 @@ UserModel = get_user_model()
 class Book(models.Model):
     title = models.CharField(max_length=15, null=False, blank=False)
     description = models.CharField(max_length=100, null=False, blank=False)
+    genre = models.CharField(max_length=15, choices=(
+        ('fiction', 'fiction'),
+        ('mystery', 'mystery'),
+        ('adventure', 'adventure'),
+        ('biography', 'biography')
+    ), null=False, blank=False)
     image_url = models.URLField(null=False, blank=False)
     creator_id = models.PositiveIntegerField(null=False, blank=False)
 
